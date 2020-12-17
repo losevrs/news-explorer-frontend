b025527d74c74c0238ba2f3503db427da416924c
Лосев Родион Станиславович <@Lizin.local>
2020-12-17 16:14:14 +0300

    feat: main bckg

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,8 +5,7 @@ import Footer from '../Footer/Footer';
 export default function Main() {
   return (
     <main className='main'>
-      <section>
-        <p>Тут будет Header и SF !!!</p>
+      <section className='main__top'>
       </section>
       <section>
         <p>Тут будет результат поиска !!!</p>
