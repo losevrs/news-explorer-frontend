ec9ad1a22a2dc582e7578b25047121a63b889663
Лосев Родион Станиславович <@Lizin.local>
2021-01-11 16:58:55 +0300

    fix: замечания рв - как я их понял. Плюс шаблон третего level-a

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,5 +1,17 @@
 function getNews(searchString) {
-  const url = `https://nomoreparties.co/news/v2/everything?language=ru&q=${searchString}&apiKey=1da83ecf9e924ecfb22223be6534e5db`;
+
+  const currentDate = new Date(); // сегодня
+  const dtms = currentDate.valueOf();
+  const days = 7;
+  const fromDate = new Date(dtms - ((24 * 3600 * 1000) * days)); // от даты
+
+  const url = `https://nomoreparties.co/news/v2/everything?` +
+    `language=ru&` +
+    `q=${searchString}&` +
+    `from=${fromDate.toISOString()}&` +
+    `to=${currentDate.toISOString()}&` +
+    `pageSize=100&` +
+    `apiKey=1da83ecf9e924ecfb22223be6534e5db`;
 
   const req = new Request(url);
 
